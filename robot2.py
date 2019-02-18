9ff05fbead77242e058670395e87f33f35e9c88e
Daniel Weber <38145815+dcweber765@users.noreply.github.com>
2019-02-18 15:45:19 -0500

    units now in meters

--- robot2.py
+++ robot2.py
@@ -336,8 +336,8 @@ def driveForward(inch):
     avgLeftSpeed = leftSpeeds/iter
     avgRightSpeed = rightSpeeds/iter
 
-    avgLeftSpeed = avgLeftSpeed*((math.pi*2.975)/(1253)) #in/sec
-    avgRightSpeed avgRightSpeed*((math.pi*2.975)/(1253)) #in/sec
+    avgLeftSpeed = avgLeftSpeed*((math.pi*0.075565)/(1253)) #m/sec
+    avgRightSpeed avgRightSpeed*((math.pi*0.075565)/(1253)) #m/sec
     time.sleep(.1)
 
     return avgLeftSpeed, avgRightSpeed
@@ -386,8 +386,8 @@ def main():
         Z = matrix([[float(accelX)], [float(accelY)], [float(gyroZ)]])
         ts = .05
         xhat, P = ekf(vl, vr, Z, ts, x_i, y_i, theta_i, P)
-        x_i = xhat.item(0) #position in inches
-        y_i = xhat.item(1) #position in inches
+        x_i = xhat.item(0) #position in meters
+        y_i = xhat.item(1) #position in meters
         theta_i = xhat.item(2) #angle in rads?
         writer.writerow({'X' : x_i, 'Y' : y_i, 'Theta' : theta_i})
     print "Done!"
