17b67a24c51cfc34374bdaab89d8cc5da8936761
Daniel Weber <38145815+dcweber765@users.noreply.github.com>
2019-02-26 00:21:48 -0500

    fixed co2 sesnor testing

--- rasPi_SpritIR_testing.py
+++ rasPi_SpritIR_testing.py
@@ -1,57 +1,63 @@
 import serial
+from datetime import datetime
 import time
 import csv
+
+
+
 
 ser0 = serial.Serial("/dev/ttyUSB0", timeout=1)
 ser1 = serial.Serial("/dev/ttyUSB1", timeout=1)
 ser2 = serial.Serial("/dev/ttyUSB2", timeout=1)
-#ser3 = serial.Serial("/dev/ttyUSB3", timeout=1)
-print ("Python Progam for SprintIR 20% Sensor Development Kit")
+ser3 = serial.Serial("/dev/ttyUSB3", timeout=1)
 
-meausrementTime = 15
-loopFreq = .1
+def getCO2Data():
+    ser0.write(b'Z\r\n')
+    resp0 = (ser0.read(10))
+    resp0 = resp0[:8]
+    fltCo20 = float(resp0[2:])
+    ser1.write(b'Z\r\n')
+    resp1 = ser1.read(10)
+    resp1 = resp1[:8]
+    fltCo21 = float(resp1[2:])
+    ser2.write(b'Z\r\n')
+    resp2 = ser2.read(10)
+    resp2 = resp2[:8]
+    fltCo22 = float(resp2[2:])
+    ser3.write(b'Z\r\n')
+    resp3 = ser3.read(10)
+    resp3 = resp3[:8]
+    fltCo23 = float(resp3[2:])
 
-loopNum = meausrementTime*(1/loopFreq)
+    Co2_0 = fltCo20 * 10
+    Co2_1 = fltCo21 * 10
+    Co2_2 = fltCo22 * 10
+    Co2_3 = fltCo23 * 10
 
-with open("CO2_SprintIR_dataSHH2.csv",mode='w') as csvfile:
-    entries = ['CO2_0', 'CO2_1','CO2_2','CO2_3']
+    return Co2_0, Co2_1, Co2_2, Co2_3
+
+
+with open("CO2_SprintIR_riseTime.csv",mode='w') as csvfile:
+    entries = ['CO2_0', 'CO2_1','CO2_2','CO2_3', 'time']
     writer = csv.DictWriter(csvfile, fieldnames=entries)
 
     writer.writeheader()
-    print (loopNum)
     # \r\n is CR and LF
     ser0.flushInput()
     ser1.flushInput()
     ser2.flushInput()
-    #ser3.flushInput()
+    ser3.flushInput()
     time.sleep(1)
     ser0.write(b'G\r\n')
     ser1.write(b'G\r\n')
     ser2.write(b'G\r\n')
-    #ser3.write(b'G\r\n')
+    ser3.write(b'G\r\n')
     print('Zero Point Cal Air')
     time.sleep(1)
 
-    for x in range(int(loopNum)):
-        ser0.write(b'Z\r\n')
-        resp0 = (ser0.read(10))
-        resp0 = resp0[:8]
-        fltCo20 = float(resp0[2:])
-        print ("CO2 PPM   =", fltCo20 *10) # multiplierZ Change data to print on a single writeRow...and figure out serial in py3
-        ser1.write(b'Z\r\n')
-        resp1 = ser1.read(10)
-        resp1 = resp1[:8]
-        fltCo21 = float(resp1[2:])
-        print ("CO2 PPM 2 =", fltCo21 *10) # multiplierZ
-        ser2.write(b'Z\r\n')
-        resp2 = ser2.read(10)
-        resp2 = resp2[:8]
-        fltCo22 = float(resp2[2:])
-        print ("CO2 PPM 3 =", fltCo22 *10) # multiplierZ
-        # ser3.write(b'Z\r\n')
-        # resp3 = ser3.read(10)
-        # resp3 = resp3[:8]
-        # fltCo23 = float(resp3[2:])
-        # print ("CO2 PPM 4 =", fltCo23 *10) # multiplierZ
-        writer.writerow({'CO2_0': fltCo20 *10, 'CO2_1': fltCo21 *10, 'CO2_2': fltCo22 *10, 'CO2_3':0})
+    for x in range(120):
+        print x
+        Co2_0, Co2_1, Co2_2, Co2_3 = getCO2Data()
+        writer.writerow({'CO2_0': CO2_0, 'CO2_1': CO2_1, 'CO2_2': CO2_2, 'CO2_3':CO2_3, 'time': str(datetime.now().time())})
         time.sleep(0.1)
+    print('Done!')
